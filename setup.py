a78ec0fe7dac2ced85590545a62dcd5edc92a792
Celia Oakley <celia.oakley@alumni.stanford.edu>
2014-09-22 16:26:52 -0700

    The Movie Database's create list API endpoint now working so removed comment.

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ def read(fname):
 
 setup(
     name = 'tmdbsimple',
-    version = '0.9.2',
+    version = '1.0.0',
     author = 'Celia Oakley',
     author_email = 'celia.oakley@alumni.stanford.edu',
     description = 'A Python wrapper for The Movie Database API v3',
     keywords = ['movie', 'the movie database', 'movie database', 'tmdb', 
                 'wrapper', 'database', 'themoviedb', 'moviedb', 'api'],
     url = 'https://github.com/celiao/tmdbsimple',
-    download_url = 'https://github.com/celiao/tmdbsimple/tarball/0.9.2',
+    download_url = 'https://github.com/celiao/tmdbsimple/tarball/1.0.0',
     packages = ['tmdbsimple'],
     long_description=read('README.rst'),
     install_requires = ['requests>=2.3.0'],
