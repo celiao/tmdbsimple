386b6de99bf95e3ffba22eed70456b54671dff59
Celia Oakley <celia.oakley@alumni.stanford.edu>
2017-03-12 13:59:53 -0700

    Change from requests==2.5.3 to requests[security]

--- setup.py
+++ setup.py
@@ -8,22 +8,20 @@ from setuptools import setup
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-# Install requests==2.5.3 to avoid InsecurePlatformWarning message
-# http://stackoverflow.com/questions/29134512/insecureplatformwarning-a-true-sslcontext-object-is-not-available-this-prevent
-# https://urllib3.readthedocs.org/en/latest/security.html#insecureplatformwarning
+# requests[security]: http://stackoverflow.com/questions/29134512/insecureplatformwarning-a-true-sslcontext-object-is-not-available-this-prevent/29202163#29202163
 setup(
     name = 'tmdbsimple',
-    version = '1.4.1',
+    version = '1.5.0',
     author = 'Celia Oakley',
     author_email = 'celia.oakley@alumni.stanford.edu',
     description = 'A Python wrapper for The Movie Database API v3',
     keywords = ['movie', 'the movie database', 'movie database', 'tmdb', 
                 'wrapper', 'database', 'themoviedb', 'moviedb', 'api'],
     url = 'https://github.com/celiao/tmdbsimple',
-    download_url = 'https://github.com/celiao/tmdbsimple/tarball/1.4.1',
+    download_url = 'https://github.com/celiao/tmdbsimple/tarball/1.5.0',
     packages = ['tmdbsimple'],
     long_description=read('README.rst'),
-    install_requires = ['requests==2.5.3'],
+    install_requires = ['requests[security]'],
     classifiers = [
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
