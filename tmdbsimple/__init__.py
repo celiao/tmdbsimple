eaba0a0fc5b897953193c45252880bf9062f32e0
Celia Oakley <celia.oakley@alumni.stanford.edu>
2017-10-22 10:15:55 -0700

    Update version to 1.8.0

--- tmdbsimple/__init__.py
+++ tmdbsimple/__init__.py
@@ -17,7 +17,7 @@ https://www.themoviedb.org/documentation/api/status-codes
 """
 
 __title__ = 'tmdbsimple'
-__version__ = '1.7.0'
+__version__ = '1.8.0'
 __author__ = 'Celia Oakley'
 __copyright__ = 'Copyright (c) 2013-2017 Celia Oakley'
 __license__ = 'GPLv3'
