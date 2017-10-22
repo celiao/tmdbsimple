d7dcd336460f0d807d367bd53e382afd2b4bfe8f
Celia Oakley <celia.oakley@alumni.stanford.edu>
2017-10-21 22:09:59 -0700

    Change all occurrences of TV Seasons id to series_id

--- tmdbsimple/base.py
+++ tmdbsimple/base.py
@@ -44,8 +44,8 @@ class TMDB(object):
     def _get_credit_id_path(self, key):
         return self._get_path(key).format(credit_id=self.credit_id)
 
-    def _get_id_season_number_path(self, key):
-        return self._get_path(key).format(id=self.series_id,
+    def _get_series_id_season_number_path(self, key):
+        return self._get_path(key).format(series_id=self.series_id,
             season_number=self.season_number)
 
     def _get_series_id_season_number_episode_number_path(self, key):
