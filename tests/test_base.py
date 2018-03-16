ef699450f84dd03de1bf8ca35fd92324f2bf136a
Bas van Gaalen <webtweakers@gmail.com>
2018-03-16 13:55:05 +0100

    Added unit tests to proof that the request function can be changed and that it works.

--- tests/test_base.py
+++ tests/test_base.py
@@ -12,8 +12,11 @@ Created by Celia Oakley on 2018-01-06
 :license: GPLv3, see LICENSE for more details.
 """
 
+import requests
+from requests import Response
 import unittest
 import tmdbsimple as tmdb
+from tmdbsimple import perform_request as original_request
 
 from tests import API_KEY, USERNAME, PASSWORD
 tmdb.API_KEY = API_KEY
@@ -22,8 +25,23 @@ tmdb.API_KEY = API_KEY
 Constants
 """
 MOVIE_ID = 103332
+MOVIE_TITLE = 'Ruby Sparks'
 MOVIEQUERY1 = 'Matrix'
 MOVIEQUERY2 = 'Star Wars'
+
+
+def my_test_request(method, url, params=None, data=None, headers=None):
+    return requests.request(
+        method, url, params=params, data=data, headers=headers)
+
+
+def my_fake_request(method, url, params=None, data=None, headers=None):
+    # returns empty OK response
+    response = Response()
+    response._content = b'{}'
+    response.status_code = 200
+    return response
+
 
 class TMDBTestCase(unittest.TestCase):
     # We want to be able to call methods multiple times.
@@ -50,3 +68,25 @@ class TMDBTestCase(unittest.TestCase):
         response = search.movie(query=MOVIEQUERY2)
         title2 = search.results[0]['original_title']
         self.assertNotEqual(title1, title2)
+
+
+class RequesterTestCase(unittest.TestCase):
+    def tearDown(self):
+        tmdb.perform_request = original_request
+
+    # Confirm that things still work with the request function replaced
+    # with our own.
+    def test_decoupled_request(self):
+        tmdb.perform_request = my_test_request
+        movie = tmdb.Movies(MOVIE_ID)
+        response = movie.info()
+        self.assertEqual(movie.title, MOVIE_TITLE)
+
+    # Confirm that things stop working with a faulty request function.
+    # Proofs that it's used.
+    def test_uncoupled_request(self):
+        tmdb.perform_request = my_fake_request
+        movie = tmdb.Movies(MOVIE_ID)
+        response = movie.info()
+        self.assertEqual(response, {})
+        self.assertFalse(hasattr(movie, 'title'))
