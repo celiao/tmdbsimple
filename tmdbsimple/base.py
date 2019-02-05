eb8d2de1d06791594eca7b5d9c6eb67c39789ba6
Matthew Fiore <matthewfiore@users.noreply.github.com>
2019-02-05 16:45:48 -0500

    update base.py to avoid 429 errors from api
    getting lots of 429 errors.
    found this and tested myself. no longer erroring out when scanning large collections.

--- tmdbsimple/base.py
+++ tmdbsimple/base.py
@@ -13,6 +13,7 @@ Created by Celia Oakley on 2013-10-31.
 
 import json
 import requests
+import time
 
 
 class APIKeyError(Exception):
@@ -72,10 +73,16 @@ class TMDB(object):
         url = self._get_complete_url(path)
         params = self._get_params(params)
 
-        response = requests.request(
-            method, url, params=params, 
-            data=json.dumps(payload) if payload else payload,
-            headers=self.headers)
+        retry = True
+        while retry:
+            retry = False
+            response = requests.request(
+                method, url, params=params, 
+                data=json.dumps(payload) if payload else payload,
+                headers=self.headers)
+        if response.status_code == 429:
+                retry = True
+                time.sleep(int(response.headers['Retry-After']))
 
         response.raise_for_status()
         response.encoding = 'utf-8'
